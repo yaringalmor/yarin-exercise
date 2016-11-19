020fe9063e07f4f2543d3540f3ce502614512065
yaringalmor <yarin_galmor@yahoo.com>
2016-11-19 23:08:28 +0200

    add comments

--- counting-panda.js
+++ counting-panda.js
@@ -3,15 +3,12 @@ function CountingPanda(){
     this.counter = 0; //GET request counter
 }
 
+//Counts get requests, and return the counter
 CountingPanda.prototype.countGetRequests = function(request){
     if (request.method === 'GET'){
         this.counter++;
         return this.counter;
     }
 }
-
-//CountingPanda.prototype.getNumOfGetRequests = function(){
-//    return this.counter;
-//}
 
 module.exports = CountingPanda;
